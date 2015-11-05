9c391962efde97a3731fcdc63911020479f4c2bc
doei <doei@users.noreply.github.com>
2015-11-05 19:33:39 +0100

    oops

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   mount_devise_token_auth_for 'User', at: 'auth'
 
-  namespace :v1, :defaults: { format: :json } do
+  namespace :v1, defaults: { format: :json } do
     resources :users, only: [:show]
   end
 end
