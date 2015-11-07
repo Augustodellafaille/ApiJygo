84c3313a23f4a659a3696d382542bec8268ebe6f
doei <doei@users.noreply.github.com>
2015-11-07 16:20:54 +0100

    order validation

--- app/models/order.rb
+++ app/models/order.rb
@@ -3,4 +3,7 @@ class Order < ActiveRecord::Base
   belongs_to :customer, :class_name => 'User', :inverse_of => :accepted_orders
   has_one :order_content
   has_one :review
+
+  validates :shopper_id, presence: true
+  validates :customer_id, presence: true
 end
