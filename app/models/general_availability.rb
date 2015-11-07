6b7a11bbe790ce8c0a8b517118b3499bb6d30d44
doei <doei@users.noreply.github.com>
2015-11-07 16:25:22 +0100

    order_content & gen_avail validations

--- app/models/general_availability.rb
+++ app/models/general_availability.rb
@@ -1,3 +1,8 @@
 class GeneralAvailability < ActiveRecord::Base
   belongs_to :user
+
+  validates :user_id, presence: true
+  validates :weekday, presence: true
+  validates :begin_time, presence: true
+  validates :end_time, presence: true
 end
--- app/models/order_content.rb
+++ app/models/order_content.rb
@@ -1,4 +1,6 @@
 class OrderContent < ActiveRecord::Base
   belongs_to :order
   has_and_belongs_to_many :products
+
+  validates :order_id, presence: true
 end
